fbed6719506c0b86ac701f3d14f040c56891aedb
Наташа Неровня <natanerovnja@gmail.com>
2016-03-15 18:11:59 +0300

    Add method for counting gcd for 3 numbers

--- EuclidAlgorithm/GCD.cs
+++ EuclidAlgorithm/GCD.cs
@@ -1,30 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EuclidAndStainAlgorithm
 {
+    /// <summary>
+    /// Class for count GCD by different algorithms
+    /// </summary>
     public class GCD
     {
-        public static int CountGCDEuclid(ref TimeSpan time, int a, int b)
+        #region Public Methods
+        /// <summary>
+        /// Count GCD by Euclid Algorithm for two numbers. In Output window you can see required time
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common denominator</returns>
+        public static int CountGCDEuclid(int a, int b)
         {
             if (a == 0 && b == 0)
                 throw new ArgumentException();
+            a = Math.Abs(a);
+            b = Math.Abs(b);
             if (a < b)
                 Swap(a, b);
+            if (a % b == 0)
+                return b;
 
             int rem = -1;
-            while (rem != 0)
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            do
             {
                 rem = a % b;
                 a = b;
-                b = rem; 
-            }
+                b = rem;
+            } while (a % b != 0);
+            sw.Stop();
 
+            Debug.WriteLine($"Required time for two numbers: {sw.Elapsed}");
             return rem;
         }
+
+        /// <summary>
+        ///  Count GCD by Euclid Algorithm for three numbers. In Output window you can see required time for counting gcd for each couple of values
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="c">Third number</param>
+        /// <returns>Greatest common denominator of three numbers</returns>
+        public static int CountGCDEuclid(int a, int b, int c)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int gcd = CountGCDEuclid(a, b);
+            int gcd2 = CountGCDEuclid(gcd, c);
+            sw.Stop();
+            Debug.WriteLine($"Required time for three numbers: {sw.Elapsed}");
+            return gcd2;
+        }
+
+        #endregion
+
+            #region Private Methods
 
         private static void Swap(int a, int b)
         {
@@ -32,5 +73,7 @@ namespace EuclidAndStainAlgorithm
             a = b;
             b = temp;
         }
+
+        #endregion
     }
 }
