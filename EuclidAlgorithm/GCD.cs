4091a79971102f40ad477623d0e4de5c288dd047
Наташа Неровня <natanerovnja@gmail.com>
2016-03-15 17:37:40 +0300

    Add Euclid algorithm for counting GCD

---
+++ EuclidAlgorithm/GCD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidAndStainAlgorithm
+{
+    public class GCD
+    {
+        public static int CountGCDEuclid(ref TimeSpan time, int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException();
+            if (a < b)
+                Swap(a, b);
+
+            int rem = -1;
+            while (rem != 0)
+            {
+                rem = a % b;
+                a = b;
+                b = rem; 
+            }
+
+            return rem;
+        }
+
+        private static void Swap(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
