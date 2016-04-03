e7e08b4d28391908937379d118f9ca7f38b6fbfe
Наташа Неровня <natanerovnja@gmail.com>
2016-04-04 00:53:56 +0300

    Add unit and NUnit tests

---
+++ EuclidAndStainAlgorithm.NUnitTests/GCDTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EuclidAndStainAlgorithm;
+using NUnit.Framework;
+
+namespace EuclidAndStainAlgorithm.NUnitTests
+{
+    public class GCDTests
+    {
+        public IEnumerable<TestCaseData> TestEuclidAndStain
+        {
+            get
+            {
+                yield return new TestCaseData(null).Throws(typeof(NullReferenceException));
+                yield return new TestCaseData(2, 4).Returns(2);
+                yield return new TestCaseData(-4, -36).Returns(4);
+                yield return new TestCaseData(0, 11).Returns(11);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestEuclidAndStain))]
+        public int CountGCD_EuclidWithYield(int a, int b)
+        {
+            double time;
+            return GCD.CountGCD(a, b, out time, GCD.EuclidAlgorithm);
+        }
+
+        [Test, TestCaseSource(nameof(TestEuclidAndStain))]
+        public int CountGCD_StainWithYield(int a, int b)
+        {
+            double time;
+            return GCD.CountGCD(a, b, out time);
+        }
+
+    }
+}
---
+++ EuclidAndStainAlgorythm.Tests/GCDTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EuclidAndStainAlgorithm;
+
+namespace EuclidAndStainAlgorythm.Tests
+{
+    [TestClass]
+    public class GCDTests
+    {
+#region Private field
+        private double time;
+#endregion
+
+        #region Test algorythm
+        [TestMethod]
+        public void DCG_Euclid_TwoPositiveNumbers()
+        {
+            int a = 21, b = 49, expected = 7;
+
+            int actual = GCD.CountGCD(a, b, out time, GCD.EuclidAlgorithm);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DCG_Euclid_OneNegative_OnePositive_Numbers()
+        {
+            int a = 21, b = -49, expected = 7;
+
+            int actual = GCD.CountGCD(a, b, out time, GCD.EuclidAlgorithm);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DCG_Euclid_PositiveNumbers_Time()
+        {
+            int[] arr = new[] { 123, 11, 45, 434 };
+            int expected = 1;
+
+            int actual = GCD.CountGCD(out time, arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DCG_Euclid_TwoNegativeNumbers()
+        {
+            int a = -21, b = -49, expected = 7;
+
+            int actual = GCD.CountGCD(a, b, out time);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void DCG_Stain_TwoPositiveNumbers()
+        {
+            int a = 21, b = 49, expected = 7;
+
+            int actual = GCD.CountGCD(a, b, out time, GCD.SteinAlgorithm);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DCG_Stain_OneNegative_OnePositive_Numbers()
+        {
+            int a = 21, b = -49, expected = 7;
+
+            int actual = GCD.CountGCD(a, b, out time, GCD.SteinAlgorithm);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DCG_Stain_TwoNegativeNumbers()
+        {
+            int a = -21, b = -49, expected = 7;
+
+            int actual = GCD.CountGCD(a, b, out time, GCD.SteinAlgorithm);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DCG_Stain_PositiveNumbers()
+        {
+            int[] arr = new[] { 4, 11, 123, -43 };
+            int expected = 1;
+
+            int actual = GCD.CountGCD(out time, GCD.SteinAlgorithm, arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+
+        #endregion 
+
+       }
+}
